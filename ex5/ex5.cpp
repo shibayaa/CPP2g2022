44c7f30fa067931cdb640939ec75c7f687206d53
Jun Shimura <junshimura@gmail.com>
2022-05-18 11:59:46 +0900

    ex5

--- ex5/ex5.cpp
+++ ex5/ex5.cpp
@@ -1,4 +1,6 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iostream>
 using namespace std;
 
 /// <summary>
@@ -6,41 +8,108 @@ using namespace std;
 /// </summary>
 class Solid {
 public:
-    virtual double GetVolume() = 0;
-    virtual double GetSurface() = 0;
+	virtual double GetVolume() = 0;
+	virtual double GetSurface() = 0;
 };
 
 /// <summary>
 /// 箱型
 /// </summary>
-class Box :Solid {
+class Box :public Solid {
 private:
-    double width;
-    double height;
-    double depth;
+	double width;
+	double height;
+	double depth;
 
 public:
-    Box(
-        double width,   //幅
-        double height,  //高さ
-        double depth) {//奥行
-        this->width = width;
-        this->height = height;
-        this->depth = depth;
-    }
-    double GetVolume() {
-        return width * height * depth;
-    }
-    double GetSurface() {
-        return (width * height + height * depth + depth * width) * 2;
-    }
-
-
+	Box(
+		double width,   //幅
+		double height,  //高さ
+		double depth    //奥行
+	) :Solid() {
+		this->width = width;
+		this->height = height;
+		this->depth = depth;
+	}
+	double GetVolume() {
+		return width * height * depth;
+	}
+	double GetSurface() {
+		return (width * height + height * depth + depth * width) * 2;
+	}
 };
 
+class Cylinder :public Solid {
+private:
+	double radius;
+	double height;
+public:
+	Cylinder(
+		double radius,  //　底面の半径
+		double height   // 高さ
+	) :Solid() {
+		this->radius = radius;
+		this->height = height;
+	}
+	double GetVolume() {
+		return radius * radius * M_PI * height;
+	}
+	double GetSurface() {
+		return radius * M_PI * 2 * (radius + height);
+	}
+};
+
+class Cone :public Solid {
+private:
+	double radius;
+	double height;
+public:
+	Cone(
+		double radius,  //　底面の半径
+		double height   // 高さ
+	) :Solid() {
+		this->radius = radius;
+		this->height = height;
+	}
+	double GetVolume() {
+		return radius * radius * M_PI * height / 3.0;
+	}
+	double GetSurface() {
+		return M_PI * radius * (radius + sqrt(radius * radius + height * height));
+	}
+};
+
+class Sphere :public Solid {
+private:
+	double radius;
+public:
+	Sphere(
+		double radius  //　底面の半径
+	) :Solid() {
+		this->radius = radius;
+	}
+	double GetVolume() {
+		return  M_PI * radius * radius * radius * 4 / 3;
+	}
+	double GetSurface() {
+		return M_PI * radius * radius * 4;
+	}
+};
+
+void DisplayVolumeSurface(Solid* solid) {
+	cout << "体積=" << solid->GetVolume() << endl;
+	cout << "表面積=" << solid->GetSurface() << endl;
+}
 int main()
 {
-    Box box{ 3,5,2.5 };
-    cout << "boxの体積="<< box.GetVolume() <<endl;
-    cout << "boxの表面積=" << box.GetSurface() << endl;
+	const int N_TABLE = 4;
+	Solid* solid[N_TABLE] = {
+		new Box(1.0,2.5,3),
+		new Cylinder(3,2),
+		new Cone(3,2),
+		new Sphere(5)
+	};
+	for (int i = 0; i < N_TABLE; i++) {
+		DisplayVolumeSurface(solid[i]);
+	}
 }
