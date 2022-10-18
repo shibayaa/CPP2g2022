647d95059b9aed2752d891e8f74631c52666690d
kurobii528@gmail.com <kurobii528@gmail.com>
2022-10-18 17:06:24 +0900

    BMI

--- list4-3-comp/list4-3-comp.cpp
+++ list4-3-comp/list4-3-comp.cpp
@@ -12,7 +12,7 @@ int main()
     double height;  // 身長
     double weight;  // 体重
     double bmi = 0;    // bmi
-    double stdWeight;   // 標準体重
+    //double stdWeight;   // 標準体重
 
     //キー入力をheightに格納する
     cout << "身長(cm)を入力してください";
@@ -23,13 +23,33 @@ int main()
     cin >> weight;
 
     // 標準体重を計算する
-    stdWeight = STD_BMI * (height / 100) * (height / 100);
+    //stdWeight = STD_BMI * (height / 100) * (height / 100);
 
     bmi = getBmi(height, weight);
     // bmiを画面に表示する
     cout << "あなたのBMIは、" << fixed << setprecision(1) << bmi << "です。" << endl;
 
     // 標準体重を画面に表示する
-    cout << "あなたの標準体重は、" << stdWeight << "です" << endl;
+    //cout << "あなたの標準体重は、" << stdWeight << "です" << endl;
+
+    if (bmi < 18.5){
+        cout << "低体重です" << endl;
+    }
+    else if(18.5 <= bmi < 25){
+        cout << "普通体重です" << endl;
+    }
+    else if(25 <= bmi < 30){
+        cout << "肥満(1度)です" << endl;
+    }
+    else if(30 <= bmi < 35){
+        cout << "肥満(2度)です" << endl;
+    }
+    else if(35 <= bmi < 40){
+        cout << "肥満(3度)です" << endl;
+    }
+    else{
+        cout << "肥満(4度)です" << endl;
+    }
+    int e;
     return 0;
 }
